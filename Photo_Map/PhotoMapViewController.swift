e92208e8e046834e23b51b1c0d8cdace12f26090
hamsikapongubala <hamsi.ruby@gmail.com>
2018-10-16 22:03:33 -0700

    Final commit

---
+++ Photo_Map/PhotoMapViewController.swift
@@ -0,0 +1,106 @@
+//
+//  PhotoMapViewController.swift
+//  Photo Map
+//
+//  Created by Nicholas Aiwazian on 10/15/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, LocationsViewControllerDelegate {
+
+  @IBOutlet weak var mapView: MKMapView!
+    var theImage: UIImage!
+  
+  
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.783333, -122.416667),
+                                       MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+  @IBAction func cameraPressed(_ sender: Any) {
+    
+      let vc = UIImagePickerController()
+      vc.delegate = self
+      vc.allowsEditing = true
+      vc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+
+      if UIImagePickerController.isSourceTypeAvailable(.camera) {
+      print("Camera is available 📸")
+      vc.sourceType = .camera
+      } else {
+      print("Camera 🚫 available so we will use photo library instead")
+      vc.sourceType = .photoLibrary
+}
+
+
+      self.present(vc, animated: true, completion: nil)
+  
+    
+  }
+  
+    func imagePickerController(_ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+
+        // Do something with the images (based on your use case)
+        theImage = originalImage
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: {
+            self.performSegue(withIdentifier: "tagSegue", sender: nil)
+        })
+}
+ 
+  
+  
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      
+      if segue.identifier == "tagSegue"{
+          let vc = segue.destination as! LocationsViewController
+          vc.delegate = self
+      }
+    }
+  
+    func locationsPickedLocation(controller: LocationsViewController, latitude: NSNumber, longitude: NSNumber) {
+      
+        let _ = navigationController?.popViewController(animated: true)
+
+        let annotation = MKPointAnnotation()
+        let locationCoordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(latitude), longitude: CLLocationDegrees(longitude))
+        annotation.coordinate = locationCoordinate
+        annotation.title = "Picture!"
+        mapView.addAnnotation(annotation)
+    }
+  
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+      
+        let reuseID = "myAnnotationView"
+      
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseID)
+        if (annotationView == nil) {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            annotationView!.canShowCallout = true
+            annotationView!.leftCalloutAccessoryView = UIImageView(frame: CGRect(x:0, y:0, width: 50, height:50))
+        }
+      
+        let imageView = annotationView?.leftCalloutAccessoryView as! UIImageView
+        imageView.image = UIImage(named: "camera")
+      
+        return annotationView
+    }
+    
+
+}
